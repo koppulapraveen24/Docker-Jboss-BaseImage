34e9cb99b761c615ab56c613a7f18eb82c98e6dc
ADAM M <scmlearningcentre@gmail.com>
2021-07-10 11:16:44 +0000

    adding docker pipeline

--- getimagescan.sh
+++ getimagescan.sh
@@ -2,20 +2,21 @@
 
 ECR_REPO=$1
 IMAGE_TAG=$2
+REGION=$3
 SCAN_STATUS=1
 
 # Check if scan is complete
 until [ "$SCAN_STATUS" -eq "0" ];
 do  
-  echo "aws ecr wait image-scan-complete --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG; echo $?"
-  SCAN_STATUS=$(aws ecr wait image-scan-complete --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG; echo $?)
+  echo "aws ecr wait image-scan-complete --region $REGION --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG"
+  SCAN_STATUS=$(aws ecr wait image-scan-complete --region $REGION --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG; echo $?)
   echo "Waiting for scan to complete"
   sleep 5
 done
 
 # Get the Scan results
-SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts')
-echo "aws ecr describe-image-scan-findings --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts'"
+echo "aws ecr describe-image-scan-findings --region $REGION --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts'"
+SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --region $REGION --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts')
 
 CRITICAL=$(echo $SCAN_FINDINGS | jq '.CRITICAL')
 HIGH=$(echo $SCAN_FINDINGS | jq '.HIGH')
